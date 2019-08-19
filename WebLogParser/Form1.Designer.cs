9e19e32e947de8e037c13f8948442f5140bba1c7
shumovan <shumovan@RC-0462>
2019-08-19 08:18:49 +0500

    Добавьте файлы проекта.

---
+++ WebLogParser/Form1.Designer.cs
@@ -0,0 +1,153 @@
+﻿namespace WebLogParser
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dtFrom = new System.Windows.Forms.DateTimePicker();
+            this.dtTo = new System.Windows.Forms.DateTimePicker();
+            this.label1 = new System.Windows.Forms.Label();
+            this.tbFolder = new System.Windows.Forms.TextBox();
+            this.btChoseFolder = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.tbDomenPath = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // dtFrom
+            // 
+            this.dtFrom.Location = new System.Drawing.Point(12, 25);
+            this.dtFrom.Name = "dtFrom";
+            this.dtFrom.Size = new System.Drawing.Size(126, 20);
+            this.dtFrom.TabIndex = 0;
+            // 
+            // dtTo
+            // 
+            this.dtTo.Location = new System.Drawing.Point(144, 25);
+            this.dtTo.Name = "dtTo";
+            this.dtTo.Size = new System.Drawing.Size(126, 20);
+            this.dtTo.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(9, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(48, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Период:";
+            // 
+            // tbFolder
+            // 
+            this.tbFolder.Location = new System.Drawing.Point(13, 68);
+            this.tbFolder.Name = "tbFolder";
+            this.tbFolder.Size = new System.Drawing.Size(516, 20);
+            this.tbFolder.TabIndex = 3;
+            // 
+            // btChoseFolder
+            // 
+            this.btChoseFolder.Location = new System.Drawing.Point(535, 68);
+            this.btChoseFolder.Name = "btChoseFolder";
+            this.btChoseFolder.Size = new System.Drawing.Size(24, 20);
+            this.btChoseFolder.TabIndex = 4;
+            this.btChoseFolder.Text = "...";
+            this.btChoseFolder.UseVisualStyleBackColor = true;
+            this.btChoseFolder.Click += new System.EventHandler(this.btChoseFolder_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(10, 52);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(77, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Путь к логам:";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(12, 94);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(126, 23);
+            this.button2.TabIndex = 9;
+            this.button2.Text = "Собрать данные";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(274, 9);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(82, 13);
+            this.label4.TabIndex = 10;
+            this.label4.Text = "Адрес домена:";
+            // 
+            // tbDomenPath
+            // 
+            this.tbDomenPath.Location = new System.Drawing.Point(277, 25);
+            this.tbDomenPath.Name = "tbDomenPath";
+            this.tbDomenPath.Size = new System.Drawing.Size(282, 20);
+            this.tbDomenPath.TabIndex = 11;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(571, 126);
+            this.Controls.Add(this.tbDomenPath);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.btChoseFolder);
+            this.Controls.Add(this.tbFolder);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.dtTo);
+            this.Controls.Add(this.dtFrom);
+            this.Name = "Form1";
+            this.Text = "Парсер лог файлов";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DateTimePicker dtFrom;
+        private System.Windows.Forms.DateTimePicker dtTo;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox tbFolder;
+        private System.Windows.Forms.Button btChoseFolder;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox tbDomenPath;
+    }
+}
+
---
+++ WebLogParser/Form1.cs
@@ -0,0 +1,180 @@
+﻿using System;
+using System.Data.OleDb;
+using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using static System.String;
+
+namespace WebLogParser
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            dtFrom.Value = new DateTime(DateTime.Today.Year, DateTime.Today.Month - 1, 15);
+            dtTo.Value = new DateTime(DateTime.Today.Year, DateTime.Today.Month, 14);
+            tbFolder.Text = Properties.Settings.Default.Path;
+            tbDomenPath.Text = IsNullOrEmpty(Properties.Settings.Default.DomenPath)
+                ? "rosneft.ru"
+                : Properties.Settings.Default.DomenPath;
+
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            Properties.Settings.Default.Path = tbFolder.Text;
+            Properties.Settings.Default.DomenPath = tbDomenPath.Text;
+            Properties.Settings.Default.Save();
+        }
+
+        private void btChoseFolder_Click(object sender, EventArgs e)
+        {
+            var fbd = new FolderBrowserDialog();;
+            fbd.SelectedPath = tbFolder.Text;
+            fbd.ShowNewFolderButton = false;
+            if (fbd.ShowDialog() == DialogResult.OK)
+            {
+                tbFolder.Text = fbd.SelectedPath;
+            }
+        }
+
+        
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (IsNullOrEmpty(tbFolder.Text))
+            {
+                MessageBox.Show(@"Выбери папку!");
+                return;
+            }
+
+            var ofd = new SaveFileDialog
+            {
+                Filter = @"Flat Files (*.csv)|*.csv",
+                FileName = Properties.Settings.Default.File
+            };
+
+            if (ofd.ShowDialog() != DialogResult.OK) return;
+            Properties.Settings.Default.File = ofd.FileName;
+            Properties.Settings.Default.Save();
+
+            var logPattern = new Dictionary<string, int>();
+            var records = new List<LogRecord>();
+
+            var nameFiles = Directory.GetFiles(tbFolder.Text, "*.log");
+            foreach (var nameFile in nameFiles)
+            {
+                var fileCreationTime = File.GetCreationTime(nameFile);
+                if (fileCreationTime < dtFrom.Value.Date || fileCreationTime > dtTo.Value.Date.AddDays(1)) continue;
+
+                var readedStrings = File.ReadAllLines(nameFile);
+
+                foreach (var s in readedStrings)
+                {
+                    if (s.StartsWith("#Fields:"))
+                    {
+                        logPattern.Clear();
+                        var t = s.Substring(9).Split(' ');
+                        for (var i = 0; i < t.Length; i++)
+                        {
+                            logPattern.Add(t[i], i);
+                        }
+                    }
+
+                    if (!s.StartsWith("#") && logPattern.Count != 0)
+                    {
+
+                        var parseStr = s.Split(' ');
+                        var date = logPattern.ContainsKey("date") ? parseStr[logPattern["date"]] : null;
+                        var time = logPattern.ContainsKey("time") ? parseStr[logPattern["time"]] : null;
+                        var ipAddr = logPattern.ContainsKey("c-ip") ? parseStr[logPattern["c-ip"]] : null;
+                        var userName = logPattern.ContainsKey("cs-username") ? parseStr[logPattern["cs-username"]] : null;
+                        var refer = logPattern.ContainsKey("cs(Referer)") ? parseStr[logPattern["cs(Referer)"]] : null;
+                        var host = logPattern.ContainsKey("cs-host") ? parseStr[logPattern["cs-host"]] : null;
+                        var status = logPattern.ContainsKey("sc-status") ? parseStr[logPattern["sc-status"]] : null;
+
+                        var record = new LogRecord();
+                        record.UserName = userName;
+                        record.IP = ipAddr;
+                        DateTime.TryParse(date, out var d);
+                        TimeSpan.TryParse(time, out var t);
+                        record.DateTime = d.Add(t);
+                        record.Reference = refer;
+                        record.Host = host;
+                        record.Status = status;
+                        record.FileName = nameFile;
+                        records.Add(record);
+                    }
+                }
+            }
+
+            var strings = records
+                .Where(r => r.UserName != "-" && !IsNullOrEmpty(r.UserName) && r.Status == "200")
+                .GroupBy(r => new {UserName = r.UserName.ToLower(), r.Host}, r => r.DateTime,
+                    (grFields, date) => new
+                    {
+                        Name = grFields.UserName,
+                        FullName = GetADInfo(tbDomenPath.Text, "sAMAccountName",
+                            grFields.UserName.Substring(grFields.UserName.IndexOf(@"\", StringComparison.Ordinal) +
+                                                        1), "name"),
+                        Host = grFields.Host,
+                        maxDate = date.Max()
+                    })
+                .OrderBy(r => r.FullName)
+                .Select(r =>
+                    r.Host + ";" + r.Name + ";" + r.FullName + ";" +
+                    r.maxDate.AddHours(5).ToString(CultureInfo.CurrentCulture))
+                .ToList();
+            strings.Insert(0, "Host;DomainAccount;DisplayName;LastAccess");
+
+            if (File.Exists(ofd.FileName)) File.Delete(ofd.FileName);
+            File.WriteAllLines(ofd.FileName, strings, Encoding.UTF8);
+            MessageBox.Show(@"Файл сохранен.", @"Инфо", MessageBoxButtons.OK, MessageBoxIcon.Information);
+
+        }
+
+        private string GetADInfo(string searchDirectory, string searchField, string searchString, string returnField)
+        {
+            var adCon = new OleDbConnection("Provider=ADSDSOObject;");
+            adCon.Open();
+            try
+            {
+                var cmd = "<LDAP://" + searchDirectory + ">;(&(objectCategory=User)" + "(" + searchField + "=" +
+                          searchString + "));" + searchField + "," + returnField + ";subtree";
+                var adCmd = new OleDbCommand(cmd, adCon);
+                var adReader = adCmd.ExecuteReader();
+                if (adReader != null && adReader.HasRows)
+                {
+                    adReader.Read();
+                    return adReader[returnField].ToString();
+                }
+
+                return "";
+            }
+            catch (Exception e)
+            {
+                adCon.Close();
+                return null;
+            }
+        }
+
+        struct LogRecord
+        {
+            public string UserName { get; set; }
+            public string IP { get; set; }
+            public DateTime DateTime { get; set; }
+            public string Reference { get; set; }
+            public string Host { get; set; }
+            public string Status { get; set; }
+            public string FileName { get; set; } 
+        }
+    }
+
+}
